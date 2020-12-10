2b707ff45021b5a23668bd9cbdfb8e65185bc310
sMenaka <dgsmenaka@gmail.com>
2020-12-10 22:27:18 +0530

    init commit

---
+++ src/main/java/com/student/studentEurekaServer/StudentEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.student.studentEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class StudentEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/student/studentEurekaServer/StudentEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.studentEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
